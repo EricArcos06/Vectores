c41fdcf5d39acd49532fe73e5dfa7cbaa294c5fe
Eric Arcos <eric.arcos@pucese.edu.ec>
2018-12-03 08:29:53 -0500

    Vectores

--- AppVectores/AppVectores/Program.cs
+++ AppVectores/AppVectores/Program.cs
@@ -11,7 +11,37 @@ namespace AppVectores
         static void Main(string[] args)
         {
             Console.WriteLine("Control de Versiones");
+
+            vectorInicializar();
             Console.ReadKey();
+        }
+        public static void vectorInicializar()
+        {
+            int[] x = new int[8];
+            x[0] = 10;
+            x[1] = 2;
+            x[2] = 20;
+            x[3] = 8;
+            x[4] = 15;
+            x[5] = 1;
+            x[6] = -9;
+            x[7] = 14;
+            //imprimir el vector
+            Console.WriteLine("\nELEMENTOS DEL VECTOR\n=============\n");
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.WriteLine("Elemento índice {0} -->{1}", (i + 1), x[i]);
+            }
+            //sumatoria de los elementos del vector
+
+            int suma = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                suma = suma + x[i];
+            }
+            Console.WriteLine("La sumatoria es {0}", suma);
+            Console.WriteLine("El promedio es {0}", suma / x.Length);
+
         }
     }
 }
